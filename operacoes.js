810e281d47d4909c1108bc2d88bd82d8c20afccf
zacajaca21 <zacajaca21@gmail.com>
2025-02-01 22:06:16 -0300

    add - animations

--- operacoes.js
+++ operacoes.js
@@ -1,4 +1,4 @@
-operacaoFracao;
+let operacaoFracao;
 
 function soma(){
     let v1 = parseFloat(document.getElementById('valor1').value);
@@ -12,11 +12,24 @@ function soma(){
     
     else{
         document.getElementById('resultado').style.opacity = '1';
-        document.getElementById('resultado').style.marginBottom = '225px';
-        document.getElementById('resultado').style.marginLeft = '900px';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s';
+        document.getElementById('resultado').style.marginBottom = '375px';
+        document.getElementById('resultado').style.marginLeft = '475px';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s, margin-left 1s';
+
+        document.getElementById('form-soma').style.marginLeft = '0';
+        document.getElementById('calcular').style.display = 'none'
+
+        document.getElementById('resultado-fracao').style.opacity = '0';
+
+        document.getElementById('form-fracao').style.opacity = '0';
     }
 }
+
+
+
+
+
+
 function subtrair(){
     let v1 = parseFloat(document.getElementById('valor1').value);
     let v2 = parseFloat(document.getElementById('valor2').value);
@@ -29,11 +42,23 @@ function subtrair(){
     }
     else{
         document.getElementById('resultado').style.opacity = '1';
-        document.getElementById('resultado').style.marginBottom = '225px';
-        document.getElementById('resultado').style.marginLeft = '900px';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s';
+        document.getElementById('resultado').style.marginBottom = '375px';
+        document.getElementById('resultado').style.marginLeft = '650px';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s, margin-left 1s';
+
+        document.getElementById('form-soma').style.marginLeft = '0';
+        document.getElementById('calcular').style.display = 'none'
+        
+        document.getElementById('resultado-fracao').style.opacity = '0';
+
+        document.getElementById('form-fracao').style.opacity = '0';
     }
 }
+
+
+
+
+
 
 function multiplicar(){
     let v1 = parseFloat(document.getElementById('valor1').value);
@@ -47,10 +72,27 @@ function multiplicar(){
     }
     else{
         document.getElementById('resultado').style.opacity = '1';
-        document.getElementById('resultado').style.marginBottom = '225px';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s';
+        document.getElementById('resultado').style.marginBottom = '375px';
+        document.getElementById('resultado').style.marginLeft = '800px';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s, margin-left 1s';
+
+        document.getElementById('form-soma').style.marginLeft = '0';
+        document.getElementById('calcular').style.display = 'none'
+
+        document.getElementById('limpar').style.top = '635px'
+
+        document.getElementById('resultado-fracao').style.opacity = '0';
+
+        document.getElementById('form-fracao').style.opacity = '0';
     }
 }
+
+
+
+
+
+
+
 function dividir(){
     let v1 = parseFloat(document.getElementById('valor1').value);
     let v2 = parseFloat(document.getElementById('valor2').value);   
@@ -64,11 +106,26 @@ function dividir(){
     }
     else{
         document.getElementById('resultado').style.opacity = '1';
-        document.getElementById('resultado').style.marginBottom = '225px';
-        document.getElementById('resultado').style.marginLeft = '900px';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s';
+        document.getElementById('resultado').style.marginBottom = '375px';
+        document.getElementById('resultado').style.marginLeft = '960px';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s, margin-left 1s';
+
+        document.getElementById('form-soma').style.marginLeft = '0';
+        document.getElementById('calcular').style.display = 'none'
+
+        document.getElementById('limpar').style.top = '635px'
+
+        document.getElementById('resultado-fracao').style.opacity = '0';
+
+        document.getElementById('form-fracao').style.opacity = '0';
     }
 }
+
+
+
+
+
+
 
 function porcentagem(){
     let v1 = parseFloat(document.getElementById('valor1').value);
@@ -82,11 +139,25 @@ function porcentagem(){
     }
     else{
         document.getElementById('resultado').style.opacity = '1';
-        document.getElementById('resultado').style.marginBottom = '225px';
-        document.getElementById('resultado').style.marginLeft = '900px';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s';
+        document.getElementById('resultado').style.marginBottom = '375px';
+        document.getElementById('resultado').style.marginLeft = '1115px';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1.5s, margin-left 1s';
+
+        document.getElementById('form-soma').style.marginLeft = '0';
+        document.getElementById('calcular').style.display = 'none'
+
+        document.getElementById('resultado-fracao').style.opacity = '0';
+
+        document.getElementById('form-fracao').style.opacity = '0';
     }
 }
+
+
+
+
+
+
+
 function fracao(){
     let n1 = parseFloat(document.getElementById('valor1').value);
     let n2 = parseFloat(document.getElementById('valor2').value);
@@ -105,16 +176,20 @@ function fracao(){
         document.getElementById('form-soma').style.transition = 'margin-left 1.75s';
 
         document.getElementById('resultado-fracao').style.opacity = '0'
-        document.getElementById('resultado-fracao').style.transition = 'opacity 1s'
+        document.getElementById('resultado-fracao').style.marginLeft = '900px'
+        document.getElementById('resultado-fracao').style.transition = 'opacity 1s, margin-left 1s'
 
         document.getElementById('resultado').style.marginBottom = '725px';
         document.getElementById('resultado').style.marginLeft = '900px';
         document.getElementById('resultado').style.opacity = '1';
         document.getElementById('resultado').style.fontSize = '75px';
         document.getElementById('resultado').innerHTML = '+';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1s, font-size 1s';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1.5s, opacity 1s, font-size 1s, margin-left 1s';
 
         document.getElementById('calcular').style.opacity = '1';
+        document.getElementById('calcular').style.right = '875px';
+        document.getElementById('calcular').style.bottom = '575px';
+        document.getElementById('calcular').style.display = 'block';
         document.getElementById('calcular').style.transition = 'opacity 0.5s';
     }
 
@@ -128,25 +203,39 @@ function fracao(){
         document.getElementById('form-soma').style.transition = 'margin-left 1.2s';
 
         document.getElementById('resultado-fracao').style.opacity = '0'
-        document.getElementById('resultado-fracao').style.transition = 'opacity 1s'
+        document.getElementById('resultado-fracao').style.marginLeft = '900px'
+        document.getElementById('resultado-fracao').style.transition = 'opacity 1s, margin-left 1s'
 
         document.getElementById('resultado').style.marginBottom = '725px';
         document.getElementById('resultado').style.marginLeft = '900px';
         document.getElementById('resultado').style.opacity = '1';
         document.getElementById('resultado').style.fontSize= '75px';
         document.getElementById('resultado').innerHTML = '-';
-        document.getElementById('resultado').style.transition = 'margin-bottom 1s, opacity 1s, font-size 1s';
+        document.getElementById('resultado').style.transition = 'margin-bottom 1s, opacity 1s, font-size 1s, margin-left 1s';
 
         document.getElementById('calcular').style.opacity = '1';
+        document.getElementById('calcular').style.right = '875px';
+        document.getElementById('calcular').style.bottom = '575px';
+        document.getElementById('calcular').style.display = 'block';
         document.getElementById('calcular').style.transition = 'opacity 0.5s';
     }
         
 }
+
+
+
+
+
+
+
 function calcular(){
     let n1 = parseFloat(document.getElementById('valor1').value);
     let n2 = parseFloat(document.getElementById('valor2').value);
     let n3 = parseFloat(document.getElementById('valor3').value);
     let n4 = parseFloat(document.getElementById('valor4').value);
+
+    let denominador = (n1 * n4) + (n2 * n2)
+    let numerador = n2 * n4
 
     document.getElementById('resultado-fracao').style.marginLeft = '450px';
     document.getElementById('resultado-fracao').style.opacity = '1';
@@ -161,25 +250,46 @@ function calcular(){
     document.getElementById('resultado').style.marginLeft = '625px';
     document.getElementById('resultado').style.transition = 'margin-left 1s';
 
-    if(isNaN(n1) || isNaN(n2) ||  isNaN(n3) || isNaN(n4) || n1 === 0 || n2 === 0 || n3 === 0 || n4 === 0){
-        
+    if(isNaN(n1) || isNaN(n2) || isNaN(n3) || isNaN(n4) || n1 === 0 || n2 === 0 || n3 === 0 || n4 === 0){
         document.getElementById('resultado-fracao').style.opacity = '1';
     }
 
     else{
         if(operacaoFracao === 1){
             document.getElementById('resultado-fracao').style.display = 'block';
-            document.getElementById('resultado-02').value = `${n4 *n2}`;
-            document.getElementById('resultado-01').value = `${(n1 * n4) + (n3 *n2)}`;
-        }
 
-        else{
+            for (let i = 2; i <= numerador && i <= denominador; i++) {
+
+                if (numerador % i === 0 && denominador % i === 0) {
+
+                    document.getElementById('resultado-01').value = `${(n1 * n4 + n3 * n2) / i}`
+                    document.getElementById('resultado-02').value = `${n4 * n2 / i}`
+                }
+            }
+        }
+        if(operacaoFracao === 2){
             document.getElementById('resultado-fracao').style.display = 'block';
-            document.getElementById('resultado-02').value = `${n4 *n2}`;
-            document.getElementById('resultado-01').value = `${(n1 * n4) - (n3 * n2)}`;
+
+            for(let x = 2; x <= numerador && x <= denominador; x++){
+                if(numerador % x === 0 && denominador % x === 0){
+
+                    let v1 = n1 * 2
+                    let v2 = n1 - v1
+
+                    document.getElementById('resultado-01').value = `${(v2 * n4 + n2 * n3) / x}`
+                    document.getElementById('resultado-02').value = `${(n2 * n4) / x}`
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
 function limpar(){
     document.getElementById('valor1').value = '0'
     document.getElementById('valor2').value = '0'
@@ -201,6 +311,7 @@ function limpar(){
     document.getElementById('form-soma').style.marginLeft = '0';
 
     document.getElementById('calcular').style.opacity = '0';
+    document.getElementById('calcular').style.display = 'none'
 
     document.getElementById('resultado-fracao').style.opacity = '0';
 }
